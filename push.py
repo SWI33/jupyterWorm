2bb6191cf82d8b57322af9147c7baa0189269344
SWISSCHEESE <59851390+SWI33@users.noreply.github.com>
2023-08-11 12:57:34 -0500

    Create push.py

---
+++ push.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse
+import websocket
+import json
+import time
+
+def send_command_to_terminal(ip, port, terminal_id, command):
+    ws_url = f"ws://{ip}:{port}/terminals/websocket/{terminal_id}"
+    ws = websocket.create_connection(ws_url)
+    
+    try:
+        ws.send(json.dumps(['stdin', command + '\n']))
+
+        # Wait for multiple responses over a short period
+        outputs = []
+        ws.settimeout(0.5)
+        end_time = time.time() + 2  # Wait for 2 seconds to collect data        
+        while time.time() < end_time:
+            try:
+                response = ws.recv()
+                data = json.loads(response)
+                outputs.append(data)
+            except websocket._exceptions.WebSocketTimeoutException:
+                # Timeout means no more data to read, so break
+                break
+
+        return outputs
+
+    except Exception as e:
+        return [str(e)]
+
+    finally:
+        ws.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Send commands to a remote terminal over WebSocket.")
+    parser.add_argument("-i", "--ip", help="IP address of the terminal server", default="143.198.160.142")
+    parser.add_argument("-p", "--port", help="Port of the terminal server", default="30000")
+    parser.add_argument("-t", "--terminal_id", help="ID of the terminal session", default="1")
+    parser.add_argument("-c", "--command", help="Command to execute", required=True)
+    
+    args = parser.parse_args()
+
+    responses = send_command_to_terminal(args.ip, args.port, args.terminal_id, args.command)
+    
+    for response in responses:
+        print(response)
